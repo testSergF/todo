ad771a339b0c5d9baecd9373f19d24abc257c108
testSergF <fsvby@rambler.ru>
2016-02-10 16:44:18 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+class Config {
+	
+	public $hostname = 'localhost';
+	public $username = 'root';
+	public $password = '';
+	public $dbname = 'todo';
+	
+	public $storage_type = 'mysql'; 	// mysql or xml
+
+}
